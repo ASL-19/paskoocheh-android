954163d3c20a32ad93c8321e3b4779e873e4ca49
Amy Jackson <amy@asl19.org>
2021-03-18 09:33:42 -0400

    Added functions to set custom certificate

--- app/src/main/java/org/asl19/paskoocheh/PaskoochehApplication.java
+++ app/src/main/java/org/asl19/paskoocheh/PaskoochehApplication.java
@@ -7,22 +7,51 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 
 import com.evernote.android.job.JobManager;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.squareup.leakcanary.LeakCanary;
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.ViewTreeObserver;
+
 import ie.equalit.ouinet.Config;
 import ie.equalit.ouinet.Ouinet;
 import org.asl19.paskoocheh.amazon.AmazonComponenet;
 import org.asl19.paskoocheh.amazon.DaggerAmazonComponenet;
 import org.asl19.paskoocheh.service.ConfigJobCreator;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.Locale;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import lombok.Getter;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
@@ -35,7 +64,14 @@ import static org.asl19.paskoocheh.Constants.OUINET_PREF;
 import static org.asl19.paskoocheh.Constants.PASKOOCHEH_PREFS;
 import static org.asl19.paskoocheh.Constants.PRIMARY_CHANNEL;
 
-public class PaskoochehApplication extends Application {
+public class PaskoochehApplication extends Application{
+    static {
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "8080");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "8080");
+    }
+
     private static final String LOGTAG = "OuinetPaskoocheh";
     public static Ouinet mOuinet;
     public static boolean USE_SERVICE = true;
@@ -47,6 +83,8 @@ public class PaskoochehApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        FirebaseApp.initializeApp(this);
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
         JobManager.create(this).addJobCreator(new ConfigJobCreator(getApplicationContext()));
 
         if (LeakCanary.isInAnalyzerProcess(this)) {
@@ -54,8 +92,10 @@ public class PaskoochehApplication extends Application {
             // You should not init your app in this process.
             return;
         }
-        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
+
         //Ouniet
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
 
         injectorCert = getResources().getString(R.string.ouinet_injector_tls_cert);
 
@@ -63,10 +103,17 @@ public class PaskoochehApplication extends Application {
                 .setCacheHttpPubKey(getResources().getString(R.string.ouinet_cache_http_pubkey))
                 .setInjectorCredentials(getResources().getString(R.string.ouinet_injector_credentials))
                 .setInjectorTlsCert(injectorCert)
-                .setTlsCaCertStorePath("cert/cacert.pem")
+                .setTlsCaCertStorePath("file:///android_asset/cert/cacert.pem")
                 .setCacheType(getResources().getString(R.string.ouinet_cache_type))
                 .build();
-
+        Log.i(LOGTAG, "TLS Store Path's:"+ouinetConfig.getInjectorTlsCertPath());
+        try {
+            File initialFile = new File(ouinetConfig.getCaRootCertPath());
+            InputStream ca = new FileInputStream(initialFile);
+            setCustomCertificateAuthority(ca);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         if (injectorCert != null) {
             Log.i(LOGTAG, "Injector's TLS certificate:");
             for (String line : injectorCert.split("\n")) {
@@ -74,7 +121,7 @@ public class PaskoochehApplication extends Application {
             }
         }
         //Log.d(LOGTAG,"ouinet"+String.valueOf(getApplicationContext().getSharedPreferences(PASKOOCHEH_PREFS, Context.MODE_PRIVATE).getBoolean(OUINET_PREF, true)));
-        if (getApplicationContext().getSharedPreferences(PASKOOCHEH_PREFS, Context.MODE_PRIVATE).getBoolean(OUINET_PREF,false)){
+        if (getApplicationContext().getSharedPreferences(PASKOOCHEH_PREFS, Context.MODE_PRIVATE).getBoolean(OUINET_PREF, false)) {
             if (USE_SERVICE) {
                 Log.d(LOGTAG, " --------- Starting ouinet service");
                 OuinetService.startOuinetService(this, ouinetConfig);
@@ -88,7 +135,6 @@ public class PaskoochehApplication extends Application {
         LeakCanary.install(this);
         // Normal app init code...
 
-
         amazonComponenet = DaggerAmazonComponenet.builder()
                 .paskoochehApplicationModule(new PaskoochehApplicationModule(getApplicationContext()))
                 .build();
@@ -99,7 +145,6 @@ public class PaskoochehApplication extends Application {
                                 .setFontAttrId(R.attr.fontPath)
                                 .build()))
                 .build());
-
 
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -114,10 +159,116 @@ public class PaskoochehApplication extends Application {
         setLocale();
     }
 
+    public static void setCustomCertificateAuthority(InputStream inputStream) {
+
+        try {
+            // Load CAs from an InputStream
+            // (could be from a resource or ByteArrayInputStream or ...)
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = new BufferedInputStream(inputStream);
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(caInput);
+                System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN() + Arrays.toString(((X509Certificate) ca).getSignature()));
+            } finally {
+                caInput.close();
+            }
+
+            // Create a KeyStore containing our trusted CAs
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            // Create a TrustManager that trusts the CAs in our KeyStore and system CA
+            UnifiedTrustManager trustManager = new UnifiedTrustManager(keyStore);
+
+                // Create an SSLContext that uses our TrustManager
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new TrustManager[]{trustManager}, null);
+            SSLContext.setDefault(context);
+            // Tell the URLConnection to use a SocketFactory from our SSLContext
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+
+
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         setLocale();
+    }
+
+    private static class UnifiedTrustManager implements X509TrustManager {
+        private X509TrustManager defaultTrustManager;
+        private X509TrustManager localTrustManager;
+
+        public UnifiedTrustManager(KeyStore localKeyStore) throws KeyStoreException {
+            try {
+                this.defaultTrustManager = createTrustManager(null);
+                this.localTrustManager = createTrustManager(localKeyStore);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private X509TrustManager createTrustManager(KeyStore store) throws NoSuchAlgorithmException, KeyStoreException {
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(store);
+
+            if (store == null) {
+                for (TrustManager tm : tmf.getTrustManagers()) {
+                    if (tm instanceof X509TrustManager) {
+                        return (X509TrustManager) tm;
+                    }
+                }
+            }
+
+            TrustManager[] trustManagers = tmf.getTrustManagers();
+            return (X509TrustManager) trustManagers[0];
+        }
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            try {
+                defaultTrustManager.checkServerTrusted(chain, authType);
+            } catch (CertificateException ce) {
+                localTrustManager.checkServerTrusted(chain, authType);
+            }
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            try {
+                defaultTrustManager.checkClientTrusted(chain, authType);
+            } catch (CertificateException ce) {
+                localTrustManager.checkClientTrusted(chain, authType);
+            }
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            X509Certificate[] first = defaultTrustManager.getAcceptedIssuers();
+            Log.i("DEFAULT", first.toString());
+            X509Certificate[] second = localTrustManager.getAcceptedIssuers();
+            Log.i("LOCAL", second.toString());
+            X509Certificate[] result = Arrays.copyOf(first, first.length + second.length);
+            System.arraycopy(second, 0, result, first.length, second.length);
+            return result;
+        }
     }
 
     private void setLocale() {
@@ -141,8 +292,9 @@ public class PaskoochehApplication extends Application {
             conf.locale = locale;
         }
     }
+
 //    @Override
-//    protected void onDestroy() {
+//    public void onDestroy() {
 //        Log.d(LOGTAG, "---------------- Destroying----------------------------");
 //        if (USE_SERVICE) {
 //            Log.d(LOGTAG, " ---- Using service, not doing anything ---");
@@ -152,5 +304,6 @@ public class PaskoochehApplication extends Application {
 //                mOuinet = null;
 //            }
 //        }
-        
+//
+//    }
 }
